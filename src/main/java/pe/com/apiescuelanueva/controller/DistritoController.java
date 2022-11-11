cc6bc64a27069cc23c3d48d9a3e5ee0c78cde270
consultora-infoticsolutions <consultora.infoticsolutions@gmail.com>
2022-11-11 13:08:22 -0500

    Modificacion DistritoController

--- src/main/java/pe/com/apiescuelanueva/controller/DistritoController.java
+++ src/main/java/pe/com/apiescuelanueva/controller/DistritoController.java
@@ -1,8 +1,14 @@
 package pe.com.apiescuelanueva.controller;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.com.apiescuelanueva.entity.DistritoEntity;
@@ -17,13 +23,43 @@ import pe.com.apiescuelanueva.service.DistritoService;
 //@RequestMapping --> permite definir el nombre del ENDPOINT
 @RequestMapping("/distrito")
 public class DistritoController {
+
     @Autowired
     private DistritoService distritoservice;
-    
+
     //@GetMapping sirve para obtener valores
     @GetMapping
-    public List<DistritoEntity> findAll(){
+    public List<DistritoEntity> findAll() {
         return distritoservice.findAll();
     }
-    
+
+    @GetMapping("/custom")
+    public List<DistritoEntity> findAllCustom() {
+        return distritoservice.findAllCustom();
+    }
+
+    //@PostMapping --> permite enviar datos
+    @PostMapping
+    public DistritoEntity add(@RequestBody DistritoEntity d) {
+        return distritoservice.add(d);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<DistritoEntity> findById(@PathVariable Long id) {
+        return distritoservice.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public DistritoEntity update(@PathVariable long id, @RequestBody DistritoEntity d) {
+        d.setCodigo(id);
+        return distritoservice.update(d);
+    }
+
+    @DeleteMapping("/{id}")
+    public DistritoEntity delete(@PathVariable long id) {
+        DistritoEntity objdistrito = new DistritoEntity();
+        objdistrito.setEstado(false);
+        return distritoservice.delete(DistritoEntity.builder().codigo(id).build());
+    }
+
 }
